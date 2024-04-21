1c53005e720e3e97f46b2ffb86c3e08af35f02d6
VuVietDuy <vuvietduy1234@gmail.com>
2024-04-22 05:48:24 +0700

    Update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Origin', "*");
   // res.setHeader('Access-Control-Allow-Origin', "https://ocop.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Methods",
